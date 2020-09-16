1972e871540dfbe3720e04ef44309284d5ce683a
xianengqi <xianengqi@gmail.com>
2020-09-17 00:23:54 +0800

    Initial Commit

---
+++ SampleDemo-douyin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleDemo-douyin
+//
+//  Created by 夏能啟 on 2020/9/17.
+//  Copyright © 2020 夏能啟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleDemo-douyin/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  SampleDemo-douyin
+//
+//  Created by 夏能啟 on 2020/9/17.
+//  Copyright © 2020 夏能啟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
