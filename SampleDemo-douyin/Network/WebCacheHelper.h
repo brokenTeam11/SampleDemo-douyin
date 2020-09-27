04151eca698f3988e8d6bf2d18c2728330733fc2
xianengqi <xianengqi@gmail.com>
2020-09-27 22:15:25 +0800

    Update WebCacheHelper.h

--- SampleDemo-douyin/Network/WebCacheHelper.h
+++ SampleDemo-douyin/Network/WebCacheHelper.h
@@ -27,19 +27,81 @@ typedef void (^WebDownloadCancelBlock)(void);
 // 查询缓存NSOperation任务和下载资源WebDownloadOperation任务合并的类
 @interface WebCombineOperation : NSObject
 // 网络资源下载取消后的回调
-@property(nonatomic, copy) WebDownloadCancelBlock cancelBlock;
+@property (nonatomic, copy) WebDownloadCancelBlock cancelBlock;
 // 查询缓存NSOperation任务
-@property(nonatomic, strong) NSOperation *cacheOperation;
+@property (nonatomic, strong) NSOperation *cacheOperation;
 // 下载网络资源任务
-@property(nonatomic, strong) WebDownloadOperation *downloadOperation;
+@property (nonatomic, strong) WebDownloadOperation *downloadOperation;
 // 取消查询缓存NSOperation任务和下载资源WebDownloadOperation任务
-- (void) cancel;
+- (void)cancel;
 @end
 
 // 处理网络资源缓存类
 @interface WebCacheHelper : NSObject
 // 单例
-+ (WebCacheHelper *) sharedWebCache;
++ (WebCacheHelper *)sharedWebCache;
+// 根据key值从内存和本地磁盘中查询缓存数据
+- (NSOperation *)queryDataFromMemory:(NSString *)key cacheQueryCompletedBlock:(WebCacheQueryCompletedBlock)cacheQueryCompletedBlock;
+// 根据key值从本地磁盘中查询缓存数据
+- (NSOperation *)queryURLFromDiskMemory:(NSString *)key cacheQueryCompletedBlock:(WebCacheQueryCompletedBlock)cacheQueryCompletedBlock;
+// 根据key值从内存和本地磁盘中查询缓存数据，所查询缓存数据包含指定文件类型
+- (NSOperation *)queryDataFromMemory:(NSString *)key cacheQueryCompletedBlock:(WebCacheQueryCompletedBlock)cacheQueryCompletedBlock extension:(NSString *)extension;
+// 存储缓存数据到内存和本地磁盘
+- (void)storeDataChace:(NSData *)data forKey:(NSString *)key;
+// 根据key值从内存和本地磁盘中查询缓存数据，所查询缓存数据包含指定文件类型
+- (NSOperation *)queryURLFromDiskMemory:(NSString *)key cacheQueryCompletedBlock:(WebCacheQueryCompletedBlock)cacheQueryCompletedBlock extension:(NSString *)extension;
 
+// 存储缓存数据到本地磁盘
+- (void)storeDataDiskCache:(NSData *)data key:(NSString *)key;
+
+- (void)storeDataDiskCache:(NSData *)data key:(NSString *)key extension:(NSString *)extension;
+
+//  清除本地磁盘缓存数据
+- (void)clearCache:(WebCacheClearCompletedBlock)cacheClearCompletedBlock;
+
+@end
+
+// 自定义网络资源下载器
+@interface WebDownloader : NSObject
+// 用于处理下载任务的NSOperationQueue队列
+@property (nonatomic, strong) NSOperationQueue *downloadConcurrentQueue;
+@property (nonatomic, strong) NSOperationQueue *downloadSerialQueue;
+
+@property (nonatomic, strong) NSOperationQueue *downloadBackgroundQueue;
+@property (nonatomic, strong) NSOperationQueue *downloadPriorityHighQueue;
+// 单例
++ (WebDownloader *)sharedDownloader;
+
+// 下载指定URL网络资源
+- (WebCombineOperation *)downloadWithURL:(NSURL *)url
+                           progressBlock:(WebDownloadProgressBlock)progressBlock
+                          completedBlock:(WebDownloadCompletedBlock)completedBlock
+                             cancelBlock:(WebDownloadCancelBlock)cancelBlock;
+
+- (WebCombineOperation *)downloadWithURL:(NSURL *)url
+                           progressBlock:(WebDownloadProgressBlock)progressBlock
+                          completedBlock:(WebDownloadCompletedBlock)completedBlock
+                             cancelBlock:(WebDownloadCancelBlock)cancelBlock
+                            isConcurrent:(BOOL)isConcurrent;
+
+- (WebCombineOperation *)downloadWithURL:(NSURL *)url
+                           responseBlock:(WebDownloadResponseBlock)responseBlock
+                           progressBlock:(WebDownloadProgressBlock)progressBlock
+                          completedBlock:(WebDownloadCompletedBlock)completedBlock
+                             cancelBlock:(WebDownloadCancelBlock)cancelBlock;
+
+- (WebCombineOperation *)downloadWithURL:(NSURL *)url
+                           responseBlock:(WebDownloadResponseBlock)responseBlock
+                           progressBlock:(WebDownloadProgressBlock)progressBlock
+                          completedBlock:(WebDownloadCompletedBlock)completedBlock
+                             cancelBlock:(WebDownloadCancelBlock)cancelBlock
+                            isConcurrent:(BOOL)isConcurrent;
+
+- (WebCombineOperation *)downloadWithURL:(NSURL *)url
+                           responseBlock:(WebDownloadResponseBlock)responseBlock
+                           progressBlock:(WebDownloadProgressBlock)progressBlock
+                          completedBlock:(WebDownloadCompletedBlock)completedBlock
+                             cancelBlock:(WebDownloadCancelBlock)cancelBlock
+                            isBackground:(BOOL)isBackground;
 
 @end
